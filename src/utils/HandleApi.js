3d957da3186c3aeab12d36e2c862854cfc62cf35
fatihyanik <fatihyanik07@gmail.com>
2023-01-14 00:42:15 +0100

    yoruldum

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = process.env.BASE_URL
-//const baseUrl = "http://localhost:5000"
+const baseUrl = "https://chatapp-backend-mlcu.onrender.com/"
 
 const getAllToDo = (setToDo) => {
     axios
