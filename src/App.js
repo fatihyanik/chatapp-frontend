5e4af436370d38a426bd879e5c56b8308a5f3437
fatihyanik <69148086+fatihyanik@users.noreply.github.com>
2023-01-13 21:26:42 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-import AddUser from './components/Users/AddUser';
+import { useEffect, useState } from "react";
+import ToDo from "./components/ToDo";
+import { addToDo, getAllToDo, updateToDo, deleteToDo } from "./utils/HandleApi";
+
 
 function App() {
+
+  const [toDo, setToDo] = useState([])
+  const [text, setText] = useState("")
+  const [isUpdating, setIsUpdating] = useState(false)
+  const [toDoId, setToDoId] = useState("")
+
+  useEffect(() => {
+    getAllToDo(setToDo)
+  }, [])
+
+  const updateMode = (_id, text) => {
+    setIsUpdating(true)
+    setText(text)
+    setToDoId(_id)
+  }
+
   return (
     <div className="App">
-      <AddUser />
+
+      <div className="container">
+
+        <h1>Our Chat App</h1>
+
+        <div className="top">
+          <input
+            type="text"
+            placeholder="Buraya yazabilirsin canim..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+
+          <div
+            className="add"
+            onClick={isUpdating ?
+              () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
+              : () => addToDo(text, setText, setToDo)}>
+            {isUpdating ? "Update" : "Add"}
+          </div>
+
+        </div>
+
+        <div className="list">
+
+          {toDo.map((item) => <ToDo
+            key={item._id}
+            text={item.text}
+            updateMode={() => updateMode(item._id, item.text)}
+            deleteToDo={() => deleteToDo(item._id, setToDo)} />)}
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
